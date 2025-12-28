ad4fedf81ce91f97e1b23492bcdb5933506443fb
zuxercoding99 <51338097+zuxercoding99@users.noreply.github.com>
2025-12-27 23:16:04 -0300

    fixed config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,8 @@
-const CONFIG = {
+window.CONFIG = {
   API_BASE_URL:
     location.hostname === "localhost"
       ? "http://localhost:8080"
       : "https://social-media-with-springboot.onrender.com",
 };
+
+window.DEFAULT_AVATAR = "img/default.png";
